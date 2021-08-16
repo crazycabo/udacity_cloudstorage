90ad05448c5cbfe803d56d2a3e2d4e485399237f
Brian Smith <crazycabo350@gmail.com>
2021-08-16 14:44:29 -0400

    Update note test class name

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/e2e/NoteTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/e2e/NoteTests.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class NotesTests {
+public class NoteTests {
 
     @LocalServerPort
     private int port;
